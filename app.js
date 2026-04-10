2f3d54358262e63cb9d295addb4f5ab96bcfc209
Daniel Meireles <dmei@jysk.com>
2026-04-10 19:26:38 +0100

    Add informational message to app.js for improved user feedback

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 // esse é o programa mais simples do mundo em JavaScript. I liked! Very Much!
 console.log('Hello World\n');
 console.warn('Alô, alô Marcianoooooo...\n');
+console.info('Opa, tudo bem? Espero que sim!\n');
